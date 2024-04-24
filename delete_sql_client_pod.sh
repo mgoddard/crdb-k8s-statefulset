e6dca0284ec92a5b9d547206563052d9e62dd988
mgoddard <mgoddard@cockroachlabs.com>
2024-04-24 10:55:01 -0400

    Switching the approach to deleting the SQL pod

--- delete_sql_client_pod.sh
+++ delete_sql_client_pod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl delete -f https://raw.githubusercontent.com/cockroachdb/cockroach/master/cloud/kubernetes/bring-your-own-certs/client.yaml
+kubectl delete pod cockroachdb-client-secure
 
