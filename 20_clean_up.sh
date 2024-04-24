d513b25a0634077485f134dd8b091ab3ccf71647
mgoddard <mgoddard@cockroachlabs.com>
2024-04-24 15:55:57 -0400

    Clean up of all the resources for the demo, except for the GKE cluster

---
+++ 20_clean_up.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Starting to tear it all down ..."
+echo ""
+read -p "Type 'Y', ENTER continue: " y_n
+y_n=${y_n:-N}
+case $y_n in
+  ([yY])
+    ;;
+  (*)
+    echo "Aborting"
+    exit 0
+    ;;
+esac
+
+echo "Deleting the Geo Tourist app ..."
+kubectl delete -f ./crdb-geo-tourist.yaml
+kubectl delete -f ./data-loader.yaml
+
+echo "Deleting the SQL client pod ..."
+kubectl delete pod cockroachdb-client-secure
+
+echo "Deleting the CockroachDB deployment ..."
+kubectl delete -f ./cockroachdb-statefulset.yaml
+kubectl delete pods,statefulsets,services,poddisruptionbudget,jobs,rolebinding,clusterrolebinding,role,clusterrole,serviceaccount,prometheus,prometheusrule,serviceMonitor -l app=cockroachdb
+
+echo "Deleting the persistent volumes and persistent volume claims ..."
+kubectl delete pv,pvc --all
+
