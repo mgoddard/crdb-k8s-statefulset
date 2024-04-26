e2421cec1735174309432a414e4eb75e49d21f9c
mgoddard <mgoddard@cockroachlabs.com>
2024-04-26 16:23:58 -0400

    Rolling upgrade now to 24.1 beta 2

--- 11_rolling_upgrade.sh
+++ 11_rolling_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting rolling upgrade of CockroachDB ..."
-kubectl patch statefulsets cockroachdb --type='json' -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "cockroachdb/cockroach-unstable:v24.1.0-beta.1" } ]'
+kubectl patch statefulsets cockroachdb --type='json' -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "cockroachdb/cockroach-unstable:v24.1.0-beta.2" } ]'
 echo "Started.  Follow progress in DB Console"
 
