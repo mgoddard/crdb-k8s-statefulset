0c0668aabc24521cd803ae5f4497dc22cecb38c3
mgoddard <mgoddard@cockroachlabs.com>
2024-05-30 13:17:43 -0400

    Only delete the env file if GKE cluster delete succeeds

--- 21_gke_delete_cluster.sh
+++ 21_gke_delete_cluster.sh
@@ -2,7 +2,5 @@
 
 . ./gke_env.sh
 
-gcloud container clusters delete $NAME --region=$REGION --quiet
-
-rm -f ./gke_env.sh
+gcloud container clusters delete $NAME --region=$REGION --quiet && rm -f ./gke_env.sh
 
