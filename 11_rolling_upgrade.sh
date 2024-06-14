a6ae2b8b01fa84dd2f130a82785a12e5fa0656ae
mgoddard <mgoddard@cockroachlabs.com>
2024-06-14 12:02:12 -0400

    Switching back to 23.2.x

--- 11_rolling_upgrade.sh
+++ 11_rolling_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting rolling upgrade of CockroachDB ..."
-kubectl patch statefulsets cockroachdb --type='json' -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "cockroachdb/cockroach-unstable:v24.1.0-beta.2" } ]'
+kubectl patch statefulsets cockroachdb --type='json' -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "cockroachdb/cockroach:v23.2.5" } ]'
 echo "Started.  Follow progress in DB Console"
 
