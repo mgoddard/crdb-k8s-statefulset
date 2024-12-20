be7f055d75533df7a05e86c4c4cc92077ebf4cc1
mgoddard <mgoddard@cockroachlabs.com>
2024-12-20 06:19:00 -0500

    Running workload for 90 minutes by default

--- 06_run_workload.sh
+++ 06_run_workload.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# In parent shell: export WORKLOAD_RUN_MINUTES=15
-runtime="${WORKLOAD_RUN_MINUTES:-10}"
+WORKLOAD_RUN_MINUTES=90
 
 echo "Initializing the workload ..."
 kubectl exec --stdin --tty cockroachdb-client-secure -- /bin/bash -c "./cockroach workload init bank 'postgres://demouser:demopasswd@cockroachdb-public:26257/bank?sslrootcert=/cockroach-certs/ca.crt'"
 
-echo "Running the workload for $runtime minutes ..."
-kubectl exec --stdin --tty cockroachdb-client-secure -- /bin/bash -c "./cockroach workload run bank --duration=${runtime}m 'postgres://demouser:demopasswd@cockroachdb-public:26257/bank?sslrootcert=/cockroach-certs/ca.crt'"
+echo "Running the workload for $WORKLOAD_RUN_MINUTES minutes ..."
+kubectl exec --stdin --tty cockroachdb-client-secure -- /bin/bash -c "./cockroach workload run bank --duration=${WORKLOAD_RUN_MINUTES}m 'postgres://demouser:demopasswd@cockroachdb-public:26257/bank?sslrootcert=/cockroach-certs/ca.crt'"
 
 echo "Cleaning up ..."
 echo "DROP DATABASE BANK;" | kubectl exec --stdin cockroachdb-client-secure -- /bin/bash -c "./cockroach sql --url 'postgres://demouser:demopasswd@cockroachdb-public:26257/bank?sslrootcert=/cockroach-certs/ca.crt'"
