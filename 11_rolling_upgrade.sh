fc922aa6511562d7e63033ec932a898b6d89fdae
mgoddard <mgoddard@cockroachlabs.com>
2024-12-20 06:21:06 -0500

    Switching to 24.3.x and 5 vCPU / 20 GB RAM

--- 11_rolling_upgrade.sh
+++ 11_rolling_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting rolling upgrade of CockroachDB ..."
-kubectl patch statefulsets cockroachdb --type='json' -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "cockroachdb/cockroach:v23.2.5" } ]'
+kubectl patch statefulsets cockroachdb --type='json' -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "cockroachdb/cockroach:v24.3.1" } ]'
 echo "Started.  Follow progress in DB Console"
 
